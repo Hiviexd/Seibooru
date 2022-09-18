c9a3c053625542ab1b04bf48daaa5d9263be4587
Hivie <rayenattia@gmail.com>
2022-09-18 02:25:59 +0100

    deprecate old console logging

--- database/index.ts
+++ database/index.ts
@@ -1,22 +1,23 @@
 import mongoose from "mongoose";
 import User from "./schemas/User";
 import Post from "./schemas/Post";
-import * as consoleLog from "../server/helpers/consoleLog";
+import { LoggerConsumer } from "../server/helpers/LoggerConsumer";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-consoleLog.blue("Database", "Starting databse connection...");
+const logger = new LoggerConsumer("database");
+
+logger.printInfo("Starting databse connection...");
 mongoose.connect(
 	`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
 	(err) => {
 		if (err)
-			return consoleLog.red(
-				"database",
+			return logger.printError(
 				"An error has occurred:\n".concat(err.message)
 			);
 
-		consoleLog.green("database", "Database connected!");
+		logger.printSuccess("Database connected!");
 	}
 );
 
--- server/helpers/consoleLog.ts
+++ server/helpers/consoleLog.ts
@@ -1,3 +1,6 @@
+//! this is deprecated in favor of ./LoggerConsumer.ts
+//! kepping this here for reference/archival purposes in case I ever need to use it again
+
 import "colors";
 import { Request } from "express";
 
